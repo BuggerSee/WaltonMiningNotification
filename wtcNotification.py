e02e563fd65fff7399b3d9d4f17e54d4bf1154d6
BuggerSee <36350532+BuggerSee@users.noreply.github.com>
2018-04-08 03:07:05 +0200

    Fixed int cast

--- wtcNotification.py
+++ wtcNotification.py
@@ -20,9 +20,11 @@ while True:
             data = r.text
             j= json.loads(data)
             r2 = requests.get('http://walchain.info:18950/api/getMinedBlocksPagination/'+miningAddress+'/1/5000')
+            r2 = requests.get('http://beelazy.de/test.php')
             data2 = r2.text
             j2= json.loads(data2)
             amount = str(j2['return_counts'])
+            print(str(j2['return_counts']))
             start=1
             time.sleep(10)
         else:
@@ -33,9 +35,10 @@ while True:
             r2 = requests.get('http://walchain.info:18950/api/getMinedBlocksPagination/'+miningAddress+'/1/5000')
             data2 = r2.text
             j2= json.loads(data2)
-            if j2['return_counts'] > amount:
+            amount2 = j2['return_counts']
+            if (int(amount2)>int(amount)):
                 amount = j2['return_counts']
-                bot.sendMessage(chatId, "Balance:" + str(j['Balance']) + " WTC\r\nBlocks mined:" +str(amount) )
+                bot.sendMessage(chatId, "Mined a new Block!!!\r\n"+"New Balance:" + str(j['Balance']) + " WTC\r\nBlocks mined:" +str(amount) )
                 time.sleep(10)
             else:
                 time.sleep(10)
