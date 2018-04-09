bf8e7dfe7c83237252d02b9d5b95c84c39f75796
BuggerSee <36350532+BuggerSee@users.noreply.github.com>
2018-04-09 22:57:31 +0200

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # WaltonMiningNotification
+
+This is a simple python script that sends you a Telegram Message when you find another Block.
+## How does it work?
+It is checking your current mined Blocks via Waltonchain API at the start.
+After 10 seconds it checks if the amount of your mined blocks increased.
+If this is the case you will receive a message in your telegram bot channel.
+
+
+
+## Installation
+
+This is a python script, so make sure you installed python. I use this script on my raspberry pi with installed Raspbian.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+sudo pip install requests
+sudo pip install telepot
+```
+
+### Telegram Setup
+```
+Please look at this link how to setup a telegram bot and how to get your chatID + telegram api key
+https://www.forsomedefinition.com/automation/creating-telegram-bot-notifications/
+```
+### Set Script autostart on Raspberry
+```
+sudo nano /etc/rc.local
+```
+#### Add line 
+```
+sudo python /home/pi/wtc.py &
+```
---
+++ wtcNotification.py
@@ -0,0 +1,48 @@
+import time
+import random
+import datetime
+import json
+import telepot
+import requests
+from telepot.loop import MessageLoop
+
+amount = 0
+start=0
+miningAddress = ''		""" Insert Mining Address """
+chatId = 			""" Insert chatID """
+telegramBot = ''		""" Insert Telegram Bot Key """
+while True:
+    try:
+        if start == 0:
+            bot = telepot.Bot(telegramBot) 
+            bot.sendMessage(chatId, "Initiated Script" ) 
+            r = requests.get('https://waltonchain.net:18950/api/getBalance/'+miningAddress)
+            data = r.text
+            j= json.loads(data)
+            r2 = requests.get('https://waltonchain.net:18950/api/getMinedBlocksPagination/'+miningAddress+'/1/5000')
+            data2 = r2.text
+            j2= json.loads(data2)
+            amount = str(j2['return_counts'])
+            start=1
+            time.sleep(10)
+        else:
+            bot = telepot.Bot(telegramBot) 
+            r = requests.get('https://waltonchain.net:18950/api/getBalance/'+miningAddress) 
+            data = r.text
+            j= json.loads(data)
+            r2 = requests.get('https://waltonchain.net:18950/api/getMinedBlocksPagination/'+miningAddress+'/1/5000')
+            data2 = r2.text
+            j2= json.loads(data2)
+            amount2 = j2['return_counts']
+            if (int(amount2)>int(amount)):
+                amount = j2['return_counts']
+                r = requests.get('https://waltonchain.net:18950/api/getBalance/'+miningAddress) 
+                data = r.text
+                j= json.loads(data)
+                bot.sendMessage(chatId, "Mined a new Block!!!\r\n"+"New Balance:" + str(j['Balance']) + " WTC\r\nBlocks mined:" +str(amount) )
+                time.sleep(10)
+            else:
+                time.sleep(10)
+    except KeyError, e:
+	print(str(e))
+        time.sleep(5)
